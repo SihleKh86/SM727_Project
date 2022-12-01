21940f880ede3ea15ed2aeebd868390108cb56bf
SihleKh86 <33133128+SihleKh86@users.noreply.github.com>
2022-12-01 14:15:22 -0500

    Delete unemployment.R

--- unemployment.R
+++
@@ -1,6 +0,0 @@
-library(haven)
-
-unempl<-read_dta("/Users/sihlekhanyile/Documents/Masters in Survey and Data Science/SM727/Final_Project/data/PALMSforLabour2020.dta")
-unempl2<-read_dta("/Users/sihlekhanyile/Documents/Masters in Survey and Data Science/SM727/Final_Project/data/qlfs_unempmduration.dta")
-
-1+!=
